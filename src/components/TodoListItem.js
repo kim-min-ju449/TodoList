4bb29918f6b27a1d114f1a086f20b82774fe7bb5
김민주 <77184157+kim-min-ju449@users.noreply.github.com>
2022-05-06 12:32:23 +0900

    check 기능 구현

--- src/components/TodoListItem.js
+++ src/components/TodoListItem.js
@@ -6,15 +6,15 @@ import {
 import cn from 'classnames';
 import './TodoListItem.css';
 
-const TodoListItem = ({todo}) =>{
-    const {text, checked} =todo;
+const TodoListItem = ({todo, onRemove, onToggle}) =>{
+    const {id, text, checked} =todo;
     return(
         <div className="TodoListItem">
-            <div className={cn('checkbox', {checked})}>
+            <div className={cn('checkbox', {checked})} onClick={() => onToggle(id)}>
                 {checked ?<FaRegSquare/> : <FaRegCheckSquare/>}
                 <div className="text">{text}</div>
             </div>
-            <div className="remove">
+            <div className="remove" onClick={() => onRemove(id)}>
                 <FaMinusCircle/>
             </div>
         </div>
